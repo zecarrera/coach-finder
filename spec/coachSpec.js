56a9253ab4af12d26d9b1052d19b0d96ef94b608
Jose Neto <jose.neto@bglgroup.co.uk>
2018-04-27 17:48:17 +0100

    Fixes broken coach specs

--- spec/coachSpec.js
+++ spec/coachSpec.js
@@ -1,52 +1,40 @@
 var request = require("request");
 
-var base_url = "https://coachfindertest.localtunnel.me"
+var base_url = "https://testcoachfinder.localtunnel.me"
 
-describe('When calling server', () => {
-    describe('with GET /', () => {
-        it("then status 200 is returned", function (done) {
-
-            request.get(base_url, function(error, response, body) {
-                    expect(response.statusCode).toBe(200);
-                    done();
-                  });
-        });
-
-        it("and returns correct message", function (done) {
-            request.get(base_url, function(error, response, body) {
-                    expect(response.body).toBe("Local tunnel is working! Path Hit: /");
-                    done();
-                  });
-        });
-    });
-
-    describe('with POST /coach', () => {
-        it("then returns with username and topic to coach", function (done) {
-            var options = {
-                method: 'POST',
-                url: `${base_url}/coach`,
-                headers:
-                {
-                    'cache-control': 'no-cache',
-                    'content-type': 'application/x-www-form-urlencoded' },
-                    form:
-                    {
-                        user_id: '999999',
-                        user_name: 'test-user',
-                        text: 'new topic 1 with 10 slots'
-                    }
-                };
-
-            request(options, function (error, response, body) {
-              if (error) throw new Error(error);
-              var slackMessage = JSON.parse(response.body);
-              expect(slackMessage.attachments[0].text).toBe("<@999999> is now listed as coach for *new topic 1* with *10* slots");
-              done();
+describe('When user sends /coach', () => {
+    let serverResponse;
+    defaultCoachOptions = (topicToCoach) => {
+        return {
+            method: 'POST',
+            url: `${base_url}/coach`,
+            headers: {
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            form: {
+                user_id: '999999',
+                user_name: 'test-user',
+                text: topicToCoach
+            }
+        };
+    };
+    describe('with message set to "new topic 1 with 10 slots"', () => {
+        beforeEach((done) => {
+            request(defaultCoachOptions("new topic 1 with 10 slots"), (error, response, body) => {
+                if (error) throw new Error(error);
+                let slackMessage = JSON.parse(response.body);
+                serverResponse = slackMessage.attachments[0];
+                done();
             });
         });
+
+        it("then returns with username and topic to coach", () => {
+            expect(serverResponse.text).toBe("<@999999> is now listed as coach for *new topic 1* with *10* slots");
+        });
     });
+});
 
     //TODO: post existing topic for user should not add repeated done
     // post existing topic different user, should not be allowed
     // reject topic if slots are not provided or valid
-});
